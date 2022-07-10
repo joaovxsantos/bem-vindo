2ed767cae43aff398c1fff017766ab05ed412c49
João Vitor <joaovitor55670@gmail.com>
2022-07-10 17:08:13 -0300

    quinto commit

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ let h1 = document.querySelector("#h1")
 let input = document.getElementsByTagName("input")[0]
 let h2 = document.querySelector("#h2")
 let date = new Date()
-
+let p = document.getElementsByTagName("p")[0]
 function tradeMomement(){
     h1.innerHTML = "Olá, " + input.value + "!"
     h2.innerHTML = "Hoje é dia " + date.getDate()  + correct(parseInt(date.getMonth() + 1)) + date.getFullYear() 
-    hora()
 }
 
 
 function hora(){
-    h2.innerHTML += "<p>" +  date.getHours() + date.getMinutes() + date.getSeconds()
+    h2.innerHTML += correctH(date.getHours()) + correctH(date.getMinutes()) + correctH(date.getSeconds())
+}
+
+
+function correctH(a){
+    if( a < 10){
+        a = "0" + a;
+    }
+    return a
 }
 
 
