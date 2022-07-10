6a3b803526e8638de5742730bf86e99fa86aaa2f
João Vitor <joaovitor55670@gmail.com>
2022-07-10 15:56:03 -0300

    quarto commit

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 let submit = document.querySelector("#enviar")
-
-
+let h1 = document.querySelector("#h1")
+let input = document.getElementsByTagName("input")[0]
+let h2 = document.querySelector("#h2")
+let date = new Date()
 
 function tradeMomement(){
-   
+    h1.innerHTML = "Olá, " + input.value + "!"
+    h2.innerHTML = "Hoje é dia " + date.getDate()  + correct(parseInt(date.getMonth() + 1)) + date.getFullYear() 
+    hora()
 }
+
+
+function hora(){
+    h2.innerHTML += "<p>" +  date.getHours() + date.getMinutes() + date.getSeconds()
+}
+
+
+function correct(a){
+    if(a < 10){
+        a = "/" + "0" + a + "/"
+    }
+    return a
+}
+
+
+
 
 
 
